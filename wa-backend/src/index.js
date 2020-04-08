94ca0a89a24bbe4e8f741f7bf41194453e0e6f49
Your Name <you@example.com>
2020-04-08 23:44:47 +0200

    commit

---
+++ wa-backend/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import storage from './memory_storage.js'
+import cors from 'cors'
+
+const app = express()  // instanciranje aplikacije
+const port = 3000  // port na kojem će web server slušati
+
+app.use(cors())
+
+app.get('/posts', (req, res) => {
+    let posts = storage.posts
+    let query = req.query
+    
+    if (query.title) {
+        posts = posts.filter(e => e.title.indexOf(query.title) >= 0)
+    }
+    
+    if (query.createdBy) {
+        posts = posts.filter(e => e.createdBy.indexOf(query.createdBy) >= 0)
+    }
+    
+    if (query._any) {
+        let pretraga = query._any
+        let pojmovi = pretraga.split(" ")
+
+        posts = posts.filter(post => {
+            let podaci = post.title + " " + post.createdBy
+            let rezultat=pojmovi.every(pojam=>{
+                return podaci.indexOf(pojam)>=0
+            })
+            return rezultat
+        })
+    }
+
+    res.json(posts)
+})
+
+app.listen(port, () => console.log(`Slušam na portu ${port}!`))
